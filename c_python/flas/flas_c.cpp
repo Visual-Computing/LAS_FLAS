04f57e6e0e034762cade6279797caed6e0fd38ea
Bluemi <bruno.schilling@protonmail.ch>
2024-11-28 18:05:51 +0100

    flas uses grid now

--- c_python/flas/flas_c.cpp
+++ c_python/flas/flas_c.cpp
@@ -69,58 +69,6 @@ std::tuple<uint32_t, uint32_t> get_size(const ssize_t n_features, const float as
 	return std::make_tuple(height, width);
 }
 
-std::tuple<int, py::array_t<int32_t> > flas_1d_features(
-  const py::array_t<float, py::array::c_style> &features,
-  const float aspect_ratio, const bool freeze_holes, const bool wrap, float radius_decay, float weight_swappable, float weight_non_swappable,
-  float weight_hole, int max_swap_positions
-) {
-  const py::buffer_info features_info = features.request();
-  const ssize_t n_features = features_info.shape[0];
-  const ssize_t dim = features_info.shape[1];
-  auto [ height, width ] = get_size(n_features, aspect_ratio);
-
-  const py::array_t<int32_t> result_indices({height, width});
-  const py::buffer_info result_indices_info = result_indices.request();
-
-  if (features_info.ndim != 2)
-    return std::make_tuple(1, result_indices);
-
-  const GridMap grid_map = init_grid_map_with_n_features(static_cast<int>(height), static_cast<int>(width), n_features);
-
-  const FlasSettings settings(
-    wrap, 0.5f, radius_decay, 1, 1.0f, weight_swappable, weight_non_swappable, weight_hole, 1.0f, max_swap_positions
-  );
-
-  bool *frozen = static_cast<bool*>(malloc(width * height * sizeof(bool)));
-  for (int i = 0; i < n_features; i++) {
-    frozen[i] = false;
-  }
-  for (int i = n_features; i < width * height; i++) {
-    frozen[i] = freeze_holes;
-  }
-
-  arrange_with_holes(
-    static_cast<const float *>(features_info.ptr),
-    static_cast<int>(dim),
-    &grid_map,
-    frozen,
-    &settings
-  );
-
-  free(frozen);
-
-  const auto res_ptr = static_cast<int32_t *>(result_indices_info.ptr);
-
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      res_ptr[y * width + x] = get(&grid_map, x, y);
-    }
-  }
-
-  return std::make_tuple(0, result_indices);
-}
-
-
 std::tuple<uint32_t, uint32_t> get_optimal_grid_size(
   uint32_t total_num_features, float aspect_ratio, uint32_t min_height, uint32_t min_width
 ) {
@@ -142,6 +90,5 @@ std::tuple<uint32_t, uint32_t> get_optimal_grid_size(
 
 PYBIND11_MODULE(flas_c_py, m) {
   m.def("flas_2d_features", &flas_2d_features);
-  m.def("flas_1d_features", &flas_1d_features);
   m.def("get_optimal_grid_size", &get_optimal_grid_size);
 }
--- c_python/tests/main.py
+++ c_python/tests/main.py
@@ -2,22 +2,23 @@
 
 from PIL import Image
 import numpy as np
-from flas import flas, apply_sorting
+from flas import flas, apply_sorting, Grid
 
-
-HEIGHT, WIDTH = 64, 64
+HEIGHT, WIDTH = 64, 128
 DIM = 3
 
 
 def test_2d():
-    features = np.random.random((HEIGHT, WIDTH, DIM)).astype(np.float32)
+    features = np.random.random((HEIGHT * WIDTH, DIM)).astype(np.float32)
+
+    grid = Grid.from_data(features)
     # print(features)
     frozen = np.zeros((HEIGHT, WIDTH)).astype(np.bool)
     frozen[:10, :10] = True
     # frozen = np.logical_not(frozen)
     # print(frozen)
 
-    sorting = flas(features, frozen, wrap=True)
+    sorting = flas(grid, wrap=True)
 
     sorted_features = apply_sorting(features, sorting)
     # print(sorted_features, sorted_features.dtype, sorted_features.shape)
@@ -29,7 +30,7 @@ def test_2d():
 def test_1d():
     features = np.random.random((HEIGHT * WIDTH, DIM)).astype(np.float32)
 
-    sorting = flas(features, aspect_ratio=16 / 9, wrap=True, freeze_holes=True)
+    sorting = flas(features, wrap=False)
     print('sorting.shape:', sorting.shape)
     print(sorting)
 
@@ -41,5 +42,5 @@ def test_1d():
 
 
 if __name__ == '__main__':
-    # test_2d()
-    test_1d()
+    test_2d()
+    # test_1d()
