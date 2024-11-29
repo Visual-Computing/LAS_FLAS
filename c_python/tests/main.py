cac8373a734c811c35b3e5169fc69ab9d73edf84
Bluemi <bruno.schilling@protonmail.ch>
2024-11-29 12:46:56 +0100

    WIP: reworking builder ids / labels

--- c_python/tests/main.py
+++ c_python/tests/main.py
@@ -2,26 +2,35 @@
 
 from PIL import Image
 import numpy as np
-from flas import flas, apply_sorting, Grid
+from flas import flas, apply_sorting, GridBuilder
 
+N_ALL_FEATURES = 10000
+QUERY_SIZE = 100
 HEIGHT, WIDTH = 64, 128
 DIM = 3
 
 
 def test_2d():
-    features = np.random.random((HEIGHT * WIDTH, DIM)).astype(np.float32)
-    ids = np.arange(HEIGHT * WIDTH)
+    all_features = np.random.random((N_ALL_FEATURES, DIM)).astype(np.float32)
+    rng = np.random.default_rng()
+    query_labels = rng.choice(a=N_ALL_FEATURES, size=QUERY_SIZE, replace=False, shuffle=False)
 
-    grid = Grid.from_data(features, ids)
-    # print(features)
-    frozen = np.zeros((HEIGHT, WIDTH)).astype(np.bool)
-    frozen[:10, :10] = True
-    # frozen = np.logical_not(frozen)
-    # print(frozen)
+    query_features = all_features[query_labels]
 
-    sorting = flas(grid, wrap=True)
+    grid_builder = GridBuilder(aspect_ratio=1.0)
+    grid_builder.put(
+        query_features[0],
+        (5, 5),
+        query_labels[0],
+    )
+    grid_builder.add(
+        features=query_features[1:],
+        labels=query_labels[1:]
+    )
 
-    sorted_features = apply_sorting(features, sorting)
+    sorting = flas(grid_builder.build(), wrap=True)
+
+    sorted_features = apply_sorting(query_features, sorting)
     # print(sorted_features, sorted_features.dtype, sorted_features.shape)
 
     image = Image.fromarray((sorted_features[:, :, :3] * 255).astype(np.uint8))
--- c_python/tests/test.py
+++ c_python/tests/test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flas import Grid
+from flas import GridBuilder
 
 
 DIM = 5
@@ -10,12 +10,12 @@ def main():
     random_features = np.random.random((290, DIM))
 
     # grid = Grid()
-    grid = Grid.from_data(grid_features)
+    grid = GridBuilder.from_data(grid_features)
     grid.add(random_features)
 
     # pos = np.arange(random_features.shape[0] * 2).reshape(random_features.shape[0], 2)
     # grid.put(random_features, pos+11)
-    random_features, taken, frozen = grid.compile(16 / 9)
+    random_features, taken, frozen = grid.build(16 / 9)
     print('random_features:', random_features.shape, ' taken:', taken.shape, '  frozen:', frozen.shape)
     print(np.sum(taken), np.sum(np.logical_not(taken)))
 
